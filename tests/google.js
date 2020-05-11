165cfb4d3cdf53398ea6f600399acaa0be8277c7
aleksandarkozelj@gmail.com <aleksandarkozelj@gmail.com>
2020-05-11 20:19:23 +0200

    correction

--- tests/google.js
+++ tests/google.js
@@ -3,6 +3,7 @@ module.exports = {
    'Google advanced search: Elon Musk' (browser) {
       const mainQuery = 'Elon Musk';
 
+      
       const mainQueryInputSelector = 'input[name="as_q"]';
       const languageDropdownOpenerSelector = '#lr_button';
       const languageDropdownValueSelector = '.goog-menuitem[value="lang_it"]';
