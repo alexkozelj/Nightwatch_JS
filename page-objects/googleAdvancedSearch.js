0be6c8ac3b2ad0085692e2c5b038f97eba9fef91
aleksandarkozelj@gmail.com <aleksandarkozelj@gmail.com>
2020-05-12 21:09:35 +0200

    test run with page object

--- page-objects/googleAdvancedSearch.js
+++ page-objects/googleAdvancedSearch.js
@@ -1,14 +1,24 @@
 module.exports = {
-   url: 'https://www.google.com/advanced_search',
+   url: 'https://www.google.com/advanced_search?hl=en',
    elements: {
        mainQueryInput: 'input[name="as_q"]',
-       languageDropdownOpener: '#lr_button',
-       languageDropdownValue: '.goog-menuitem[value="lang_it"]',
-       lastUpdateDropdownOpener: '#as_qdr_button',
-       lastUpdateDropdownValue: '.goog-menuitem[value="m"]',
-       submitButton: '.jfk-button[type="submit"]'
+       languageDropdown: '#lr_button',
+       lastUpdateDropdown: '#as_qdr_button',
+       submitButton: '.jfk-button[type="submit"]',
    },
    commands: [{
-
+       setQuery(value) {
+           return this
+               .setValue('@mainQueryInput', value);
+       },
+       selectFilter(selector, value) {
+           return this
+               .click(selector)
+               .click(`.goog-menuitem[value="${value}"]`);
+       },
+       search() {
+           return this
+               .click('@submitButton');
+       }
    }]
-}
+};
